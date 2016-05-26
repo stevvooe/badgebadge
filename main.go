0c76aadef970353fca00c8acd70bd980f4a1cb04
French Ben <frenchben@docker.com>
2016-05-25 17:43:01 -0700

    Added webserver to server valide badge based on badge count
    Signed-off-by: French Ben <frenchben@docker.com>

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/kataras/iris"
+)
+
+func init() {
+	logLevel := os.Getenv("DEBUG")
+	logrus.Infof("Logs: %v", logLevel)
+	if logLevel != "" {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+	badgeMinimum, _ = strconv.Atoi(os.Getenv("BADGE_COUNT"))
+	if badgeMinimum == 0 {
+		badgeMinimum = 3
+	}
+}
+
+func main() {
+	iris.Get("/github.com/:username/:reponame", func(ctx *iris.Context) {
+		username := ctx.Param("username")
+		reponame := ctx.Param("reponame")
+		badge, err := checkBadges(username, reponame)
+		if err != nil {
+			ctx.Write(err.Error())
+			ctx.SetStatusCode(iris.StatusInternalServerError)
+		}
+		ctx.ServeFile(badge, false)
+	})
+
+	logrus.Info("Server listening on :8080")
+	iris.Listen(":8080")
+
+}
---
+++ validate.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+
+	"github.com/Sirupsen/logrus"
+)
+
+var badgeMinimum int
+
+const inadequateBadge = "badges/inadequate.svg"
+const adequateBadge = "badges/adequate.svg"
+
+var badgeRex = regexp.MustCompile("(?i)(\\[!\\[[a-zA-Z0-9_ ]*\\]\\([0-9a-z.\\/:?=-]*\\)\\]\\([0-9a-z.\\/:-]*\\))")
+
+func checkBadges(username string, reponame string) (string, error) {
+	resp, err := http.Get(fmt.Sprintf("https://raw.githubusercontent.com/%s/%s/master/README.md", username, reponame))
+	if err != nil {
+		// handle error
+		logrus.Fatalf("could not fetch: %v", err)
+	}
+	logrus.Debug("resp: ", resp)
+	if resp.StatusCode != 200 {
+		logrus.Debugf("Github repo %s not found", fmt.Sprintf("github.com/%s/%s", username, reponame))
+		return "", errors.New("github repo not found")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+		logrus.Debugf("could not read body from %s: %v", fmt.Sprintf("github.com/%s/%s", username, reponame), err)
+		return "", err
+	}
+	logrus.Debug("body: ", string(body))
+
+	// The `All` variants of these functions apply to all
+	// matches in the input, not just the first. For
+	// example to find all matches for a regexp.
+	badgeMatch := badgeRex.FindAllString(string(body), -1)
+	logrus.Debug(badgeMatch)
+	logrus.Infof("Badge count: %v - min: %v", len(badgeMatch), badgeMinimum)
+	if len(badgeMatch) > badgeMinimum {
+		logrus.Info("Congrats you haz all badges")
+		return adequateBadge, nil
+	}
+	logrus.Info("Needz moar badges")
+	return inadequateBadge, nil
+}
