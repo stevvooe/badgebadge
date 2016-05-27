396f720e0b7b417332688cc100cb6e20f6212c72
French Ben <frenchben@docker.com>
2016-05-26 22:55:45 -0700

    Added support for light debugging of badges
    Signed-off-by: French Ben <frenchben@docker.com>

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"html/template"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/kataras/iris"
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday"
 )
 
 func init() {
@@ -20,18 +24,42 @@ func init() {
 	}
 }
 
+type Badge struct {
+	BadgesHTML template.HTML
+	Badge      string
+}
+
 func main() {
+	iris.Static("/badges", "./badges/", 1)
 	iris.Get("/github.com/:username/:reponame", func(ctx *iris.Context) {
 		username := ctx.Param("username")
 		reponame := ctx.Param("reponame")
 		queryString := ctx.URI().QueryArgs()
-		logrus.Debug("branch: %v", string(queryString.Peek("branch")))
-		badge, err := checkBadges(username, reponame, string(queryString.Peek("branch")))
+		branch := string(queryString.Peek("branch"))
+		debug := string(queryString.Peek("debug"))
+
+		logrus.Debug("branch: %v - debug: %v", branch, debug)
+		badges, err := checkBadges(username, reponame, branch)
 		if err != nil {
 			ctx.Write(err.Error())
 			ctx.SetStatusCode(iris.StatusInternalServerError)
 		}
-		ctx.ServeFile(badge, false)
+		if debug == "true" {
+			htmlBadges := blackfriday.MarkdownBasic([]byte(strings.Join(badges[1:], "\n")))
+			htmlBadges = bluemonday.UGCPolicy().SanitizeBytes(htmlBadges)
+			if len(htmlBadges) == 0 {
+				htmlBadges = []byte("<p><em>No badges found in README.md</em></p>")
+			}
+
+			if err := ctx.Render("index.html", Badge{
+				BadgesHTML: template.HTML(string(htmlBadges)),
+				Badge:      badges[0],
+			}); err != nil {
+				logrus.Panic(err)
+			}
+		} else {
+			ctx.ServeFile(badges[0], false)
+		}
 	})
 
 	logrus.Info("Server listening on :8080")
