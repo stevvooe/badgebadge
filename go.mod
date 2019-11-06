cb5be11f928e08dd0f052371b03ce22cf684a999
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2019-11-06 23:43:04 +0200

    update to Iris version 12

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/stevvooe/badgebadge
+
+go 1.13
+
+require (
+	github.com/kataras/golog v0.0.9
+	github.com/kataras/iris/v12 v12.0.1
+	github.com/microcosm-cc/bluemonday v1.0.2
+	github.com/russross/blackfriday v2.0.0+incompatible
+)
--- main.go
+++ main.go
@@ -6,18 +6,20 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/Sirupsen/logrus"
-	"github.com/kataras/iris"
+	"github.com/kataras/golog"
+	"github.com/kataras/iris/v12"
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday"
 )
 
 func init() {
 	logLevel := os.Getenv("DEBUG")
-	logrus.Infof("Logs: %v", logLevel)
 	if logLevel != "" {
-		logrus.SetLevel(logrus.DebugLevel)
+		golog.Infof("Logs: %v", logLevel)
+		// default golog instance is shared with Iris too.
+		golog.SetLevel("debug")
 	}
+
 	badgeMinimum, _ = strconv.Atoi(os.Getenv("BADGE_COUNT"))
 	if badgeMinimum == 0 {
 		badgeMinimum = 3
@@ -30,43 +32,44 @@ type Badge struct {
 }
 
 func main() {
-	iris.Static("/badges", "./badges/", 1)
-	iris.Get("/github.com/:username/:reponame", handleReport)
-	iris.Get("/report/github.com/:username/:reponame", handleReport)
-	// logrus.Debug("Server listening on :8080")
-	iris.Listen(":8080")
+	app := iris.New()
+	app.RegisterView(iris.HTML("./templates", ".html"))
+	app.HandleDir("/badges", "./badges/")
+
+	app.Get("/github.com/{username}/{reponame}", handleReport)
+	app.Get("/report/github.com/{username}/{reponame}", handleReport)
+
+	app.Run(iris.Addr(":8080"))
 }
 
-func handleReport(ctx *iris.Context) {
-	username := ctx.Param("username")
-	reponame := ctx.Param("reponame")
-	queryString := ctx.URI().QueryArgs()
-	branch := string(queryString.Peek("branch"))
-	debug := string(queryString.Peek("debug"))
-	uri := string(ctx.RequestURI())
-	pathSlice := strings.Split(uri, "/")
+func handleReport(ctx iris.Context) {
+	username := ctx.Params().Get("username")
+	reponame := ctx.Params().Get("reponame")
+	branch := ctx.URLParam(("branch"))
+	debug := ctx.URLParam("debug")
+	pathSlice := strings.Split(ctx.Path(), "/")
 	if pathSlice[1] == "report" {
 		debug = "true"
 	}
 
-	logrus.Debug("branch: %v - debug: %v", branch, debug)
+	golog.Debug("branch: %v - debug: %v", branch, debug)
 	badges, err := checkBadges(username, reponame, branch)
 	if err != nil {
-		ctx.Write(err.Error())
-		ctx.SetStatusCode(iris.StatusInternalServerError)
+		ctx.StatusCode(iris.StatusInternalServerError)
+		ctx.WriteString(err.Error())
 	}
 	if debug == "true" {
-		htmlBadges := blackfriday.MarkdownBasic([]byte(strings.Join(badges[1:], "\n")))
+		htmlBadges := blackfriday.Run([]byte(strings.Join(badges[1:], "\n")))
 		htmlBadges = bluemonday.UGCPolicy().SanitizeBytes(htmlBadges)
 		if len(htmlBadges) == 0 {
 			htmlBadges = []byte("<p><em>No badges found in README.md</em></p>")
 		}
 
-		if err := ctx.Render("index.html", Badge{
+		if err := ctx.View("index.html", Badge{
 			BadgesHTML: template.HTML(string(htmlBadges)),
 			Badge:      badges[0],
 		}); err != nil {
-			logrus.Panic(err)
+			golog.Fatal(err)
 		}
 	} else {
 		ctx.ServeFile(badges[0], false)
